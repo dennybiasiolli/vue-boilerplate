2854a1222e287ca129f72872effef14fb3c95bb0
Denny Biasiolli <denny.biasiolli@gmail.com>
2017-12-17 12:16:36 +0100

    webpack: enabling UglifyJsPlugin for production build

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const baseWebpackConfig = require('./webpack.base.conf')
 
 const prodWebpackConfig = Object.assign({}, baseWebpackConfig);
@@ -6,6 +7,15 @@ const prodWebpackConfig = Object.assign({}, baseWebpackConfig);
 prodWebpackConfig.plugins.push(
   new webpack.DefinePlugin({
     'process.env.NODE_ENV': '"production"'
+  }),
+  new UglifyJsPlugin({
+    uglifyOptions: {
+      compress: {
+        warnings: false
+      }
+    },
+    sourceMap: true,
+    parallel: true
   }),
   // keep module.id stable when vender modules does not change
   new webpack.HashedModuleIdsPlugin(),
