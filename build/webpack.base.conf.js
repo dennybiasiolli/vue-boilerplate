431a92c44f2a6bc1d20bdb5aa87d868af35693df
Denny Biasiolli <denny.biasiolli@gmail.com>
2017-12-17 14:30:44 +0100

    webpack: using root path resolver as '@'

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   resolve: {
     alias: {
+      '@': path.join(__dirname, '../src'),
       'vue$': 'vue/dist/vue.esm.js'
     }
   },
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import { app } from './app'
-import { app2 } from './app2'
-import { app3 } from './app3'
-import { app4 } from './app4'
-import { app5 } from './app5'
-import { app6 } from './app6'
-import { app7 } from './app7'
+import { app } from '@/app'
+import { app2 } from '@/app2'
+import { app3 } from '@/app3'
+import { app4 } from '@/app4'
+import { app5 } from '@/app5'
+import { app6 } from '@/app6'
+import { app7 } from '@/app7'
 
 export default { app, app2, app3, app4, app5, app6, app7 }
