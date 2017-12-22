1f7e882c1bdad3f116ec64afe1965ac0311206ab
Denny Biasiolli <denny.biasiolli@gmail.com>
2017-12-22 20:38:00 +0100

    using a single App file to render the app

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <comp1 />
+    <comp2 />
+    <comp3 />
+    <comp4 />
+    <comp5 />
+    <comp6 />
+    <comp7 />
+  </div>
+</template>
+
+<script>
+import comp1 from '@/components/comp1'
+import comp2 from '@/components/comp2'
+import comp3 from '@/components/comp3'
+import comp4 from '@/components/comp4'
+import comp5 from '@/components/comp5'
+import comp6 from '@/components/comp6'
+import comp7 from '@/components/comp7'
+
+export default {
+  name: 'App',
+  components: {
+    comp1: comp1,
+    comp2: comp2,
+    comp3: comp3,
+    comp4: comp4,
+    comp5: comp5,
+    comp6: comp6,
+    comp7: comp7
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/index.js
+++ src/index.js
@@ -1,22 +1,8 @@
 import Vue from 'vue'
+import App from './App'
 
-import comp1 from '@/components/comp1'
-import comp2 from '@/components/comp2'
-import comp3 from '@/components/comp3'
-import comp4 from '@/components/comp4'
-import comp5 from '@/components/comp5'
-import comp6 from '@/components/comp6'
-import comp7 from '@/components/comp7'
-
-export const mainApp = new Vue({
+/* eslint-disable no-new */
+new Vue({
   el: '#main-app',
-  components: {
-    comp1: comp1,
-    comp2: comp2,
-    comp3: comp3,
-    comp4: comp4,
-    comp5: comp5,
-    comp6: comp6,
-    comp7: comp7
-  }
+  render: h => h(App)
 })
