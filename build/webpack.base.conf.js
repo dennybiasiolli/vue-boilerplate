0414dad54ac72ad9bfe2fe301fed769f76da948f
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-03-10 10:27:26 +0100

    removing deprecated CommonsChunkPlugin in webpack4

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -30,20 +30,6 @@ module.exports = {
   },
   plugins: [
     // keep module.id stable when vendor modules does not change
-    new webpack.HashedModuleIdsPlugin(),
-    // split vendor js into its own file
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks (module) {
-        // any required modules inside node_modules are extracted to vendor
-        return (
-          module.resource &&
-          /\.js$/.test(module.resource) &&
-          module.resource.indexOf(
-            path.join(__dirname, '../node_modules')
-          ) === 0
-        )
-      }
-    })
+    new webpack.HashedModuleIdsPlugin()
   ]
 }
