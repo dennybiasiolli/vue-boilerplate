fa070a705a1b098f7fceca4a6b9a8e2757a48cb3
Denny Biasiolli <denny.biasiolli@gmail.com>
2018-03-18 22:46:06 +0100

    app: using Nav component

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      :value="drawer"
-      fixed
-      app
-      @input="toggleDrawer">
-      <v-toolbar flat>
-        <v-list>
-          <v-list-tile>
-            <v-list-tile-title class="title">
-              Application
-            </v-list-tile-title>
-          </v-list-tile>
-        </v-list>
-      </v-toolbar>
-      <v-divider />
-      <v-list>
-        <v-list-tile @click="$router.push('/')">
-          <v-list-tile-action>
-            <v-icon>dashboard</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>Home</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <v-list-tile
-          v-for="(c, i) in components"
-          :key="i"
-          @click="$router.push(c.url)">
-          <v-list-tile-action>
-            <v-icon>{{ c.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ c.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    <Nav />
     <Toolbar />
     <v-content>
       <v-container fluid>
@@ -52,38 +16,18 @@
 </template>
 
 <script>
-import { createNamespacedHelpers } from 'vuex'
+import Nav from './components/Nav'
 import Toolbar from './components/Toolbar'
-
-const { mapState, mapMutations } = createNamespacedHelpers('a')
 
 export default {
   name: 'App',
   components: {
-    Toolbar
+    Toolbar,
+    Nav
   },
-  data: () => ({
-    components: [
-      { title: 'Component 1', url: '/comp1', icon: 'polymer' },
-      { title: 'Component 2', url: '/comp2', icon: 'polymer' },
-      { title: 'Component 3', url: '/comp3', icon: 'polymer' },
-      { title: 'Component 4', url: '/comp4', icon: 'polymer' },
-      { title: 'Component 5', url: '/comp5', icon: 'polymer' },
-      { title: 'Component 6', url: '/comp6', icon: 'polymer' },
-      { title: 'Component 7', url: '/comp7', icon: 'polymer' },
-      { title: 'Dashboard', url: '/dashboard', icon: 'dashboard' }
-    ]
-  }),
-  computed: Object.assign(
-    {
-      year: () => new Date().getFullYear()
-    },
-    mapState(['drawer'])
-  ),
-  methods: Object.assign(
-    {},
-    mapMutations(['toggleDrawer'])
-  )
+  computed: Object.assign({
+    year: () => new Date().getFullYear()
+  })
 }
 </script>
 
--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,42 +1,13 @@
-import Vuex from 'vuex'
-import { shallow, createLocalVue, RouterLinkStub } from '@vue/test-utils'
+import { shallow, createLocalVue } from '@vue/test-utils'
 import App from '@/App'
 
 const localVue = createLocalVue()
-localVue.use(Vuex)
-
-const state = {
-  drawer: null
-}
-
-// Mock the `increment` mutation to make it
-// possible to check if it was called.
-let mutations
-
-// This function creates a new Vuex store
-// instance for every new test case.
-const createStore = () => {
-  mutations = {
-    toggleDrawer: jest.fn().mockReturnValue(false)
-  }
-  return new Vuex.Store({
-    modules: {
-      a: {
-        namespaced: true,
-        state,
-        mutations
-      }
-    }
-  })
-}
 
 describe('App', () => {
   test('should match snapshot', () => {
     const component = shallow(App, {
       localVue,
-      store: createStore(),
       stubs: {
-        RouterLink: RouterLinkStub,
         RouterView: true
       }
     })
@@ -47,20 +18,6 @@ describe('App', () => {
   // the raw component options
   test('sets the correct components data', () => {
     expect(App.name).toBe('App')
-    expect(typeof App.data).toBe('function')
-    const defaultData = App.data()
-    expect(defaultData.components).toEqual([
-      { title: 'Component 1', url: '/comp1', icon: 'polymer' },
-      { title: 'Component 2', url: '/comp2', icon: 'polymer' },
-      { title: 'Component 3', url: '/comp3', icon: 'polymer' },
-      { title: 'Component 4', url: '/comp4', icon: 'polymer' },
-      { title: 'Component 5', url: '/comp5', icon: 'polymer' },
-      { title: 'Component 6', url: '/comp6', icon: 'polymer' },
-      { title: 'Component 7', url: '/comp7', icon: 'polymer' },
-      { title: 'Dashboard', url: '/dashboard', icon: 'dashboard' }
-    ])
-    expect(typeof App.computed.drawer).toBe('function')
     expect(App.computed.year()).toBe(new Date().getFullYear())
-    expect(typeof App.methods.toggleDrawer).toBe('function')
   })
 })
